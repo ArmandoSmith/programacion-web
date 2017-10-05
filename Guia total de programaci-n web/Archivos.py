4515cdaf6fadaff44e24c26a9dde2bb394d04ff3
Smith <31020266+ArmandoSmith@users.noreply.github.com>
2017-10-04 17:00:04 -0700

    Update Archivos.py

--- Guia total de programaci-n web/Archivos.py
+++ Guia total de programaci-n web/Archivos.py
@@ -1,64 +1,93 @@
-#Extender modelos de usuarios en jango:
+'''
+   Se asumira de antemano que se tiene instalado el ambiente virtual y la version de python/Django que prefieran.
+   Todo lo que esta entre <> (signos de mayor, menor que) se tiene que reemplazar.
+'''
 
-#1.- Generar un modelo en django:
+# ==========================================================================
+# 01.-Crear el proyecto:
+# ==========================================================================
 
-from django.contrib.auth.models import User
+(ambiente_virtual)$django-admin.py startproject nombreProyecto
 
-class Ususarios(model.Model):
-  name = models.OneToOneField(User)
-  phone = models.IntegerField()
-  email = models.CharField(max_length=48)
-  aaddress = models.CharField(max_length=256)
+# ==========================================================================
+# 02.-Crear la aplicación:
+# ==========================================================================
+
+(ambiente_virtual)$python manage.py starapp nombreApp
+
+# ==========================================================================
+# 03.-Configurar el archvio settings.py:
+# ==========================================================================
+
+setting.py
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        # ------------------------------------------------------
+        # Agregar la ruta en donde esta la carpeta de templates:
+        # ------------------------------------------------------
+        'DIRS': ["<ruta>"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+INSTALED_APPS = (
+  'django.contrib.admin',
+  'django.contrib.auth',
+  'django.contrib.contenttypes',
+  'django.contrib.sessions',
+  'django.contrib.messages',
+  'django.contrib.staticfiles',
+  # ------------------------------------------------------
+  # Agregar la app:
+  # ------------------------------------------------------
+  '<nombreApp>',
+)
+
+# ==========================================================================
+# 04.- Crear una vista:
+# ==========================================================================
+
+views.py
+
+from django.shortcuts import render
+
+def <nombreVista>(request):
+  return render(request, '<nombreArchivo>.html', {})
   
-#2.- Importar el modelo (archivo admin site):
+# ==========================================================================
+# 05.- Modificar el archivo del proyecto urls.py:
+# ==========================================================================
 
+urls.py
 
-#3.- Crear el formulario:
+from django.conf.urls import include, url
+from django.contrib import admin
 
-from django.contrib.auth.fomrs import UserCreationForm
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'', include('<nombreApp>.urls')),
+]
 
-class Users_Form(UserCreationForm):
-  phone = forms.IntegerField()
-  email = forms.CharField(max_length=48)
-  addres = forms.CharField(max_legth=256)
-  
-#4.- Crear la vista:
+# ==========================================================================
+# 06.- Craar y configurar el archivo de la aplicacin urls.py:
+# ==========================================================================
 
-from django.views.generic
+urls.py
 
-class SignUp(FormView):
-  template_name = 'home/signup.html'
-  form_class = UsersForm
-  success_url = reverse_lazy('login')
-  
-  def form_valid(self, form)
-    user = form.svae()
-    p = Usuarios()
-    p.name = user
-    p.phone = form.cleaned_data['phone']
-    p.email = form.cleaned_data['email']
-    p.address = form.cleaned_data['address']
-    p.save()
-    return super(SignUp, self).form_valid(form)
+from django.conf.urls import include, url
+from . import views
 
-#5.-Generar el template:
-
-{% extends 'base.html' %}
-{% block title %}SignUp{% endblock %}
-{% block content %}
-<form method="POST"
-  {%csrf_token %}
-  {{ form.as_p }}
-  <input tupe="submit" value="SignUp">
-  <input tupe="reset" value="Cancel">
-  
-{% endblock %}
-
-#6.-Crear la url:
-url(r'^signup/$', views.SignUp.as_view(), naame="SingUp_view")
-
-#7.-Realizar la migracion:
-#En la terminal: colocarse en la carpeta del proyecto:
-
-python manage.py make migration
-python manage.py migrate
+urlpatterns = [
+    url(r'^$', views.<urlBase>), # Esta esla url que se abrira por defecto en el servidor:
+    url(r'^<Url>/$', views.<nombreVista>, name="<nombreUrl>"), # Esta es una url normal
+]
